5984f5a67fd46913e17f12eb592dd81f2192f2fc
Andrey <tishok@inbox.ru>
2023-01-20 15:57:54 +0300

    Сделал Таск04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Из трех чисел сравниваем наибольшее");
+
+Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье  число");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+int max = numberOne;
+if ( numberTwo> max){
+    max = numberTwo;
+}
+if(numberThree > max){
+    max = numberThree;
+}
+Console.WriteLine($"Наибольшее из перечисленных чисел это {max}");
